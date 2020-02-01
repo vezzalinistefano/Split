4fdab3aa3ed4523fe6d423e64fa065fbf19d90c2
Vezzalinistefano <vezzalini.stefano@yahoo.it>
2020-02-01 17:35:18 +0100

    MergeTask inizializzato

--- logic/MergeTask.java
+++ logic/MergeTask.java
@@ -4,4 +4,17 @@ import java.io.File;
 
 public class MergeTask extends Task{
     private File[] files;
+
+    private String keyword;
+
+    public MergeTask(File[] files, String keyword) {
+        super();
+
+        this.files = files;
+        this.keyword = keyword;
+    }
+
+    public void performTask() {
+
+    }
 }
